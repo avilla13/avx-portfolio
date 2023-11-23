6c484cb7d89310dcaee41207aa45f9432b2f6e7b
avilla13 <xetox13@gmail.com>
2023-11-22 20:36:21 -0600

    Add projects with dynamic image covers

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,37 +1,45 @@
 import React from 'react'
-import './Projects.css';
+import HomeWodImage from '../../assets/HomeWod.png';
+import PlanItImage from '../../assets/PlanIt-add.png';
+import ProjectHubImage from '../../assets/ProjectHub.png';
+import MemoryDashImage from '../../assets/MemoryDash.png';
 
 export default function Projects() {
   const projectData = [
     {
       title: "MyWod",
       description: "A fitness site that provides users AI generated CrossFit workouts (aka wods).",
-      image: "path_to_image_1.jpg",
+      image: "",
       link: "https://my-wod-5a1017257b43.herokuapp.com/",
       github: "https://github.com/avilla13/MyWOD"
     },
     {
       title: "ProjectHub",
       description: "A productivity site designed to help teams successfully complete projects. This was a collaborative effort built in one-week sprint.",
-      image: "path_to_image_2.jpg",
+      image: "",
       link: "https://projecthub-avah-f1fe7fefab0a.herokuapp.com/",
       github: "https://github.com/AndrewDHulse/ProjectHub"
     },
     {
       title: "Plan-It",
       description: "An event planning website designed to simplify the process of creating and managing events.",
-      image: "path_to_image_2.jpg",
+      image: "",
       link: "https://github.com/avilla13/Plan-It",
       github: "https://plan-it-av-0a1ac1929ddc.herokuapp.com/"
     },
     {
       title: "Memory Dash",
       description: "A browser concentration game of fast-paced memory matching.",
-      image: "path_to_image_4.jpg",
+      image: "",
       link: "https://avilla13.github.io/Concentration-Memory/",
       github: "https://github.com/avilla13/Concentration-Memory"
     }
   ];
+
+  projectData[0].image = HomeWodImage;
+  projectData[1].image = ProjectHubImage;
+  projectData[2].image = PlanItImage;
+  projectData[3].image = MemoryDashImage;
 
   return (
     <div className="projects-container">
