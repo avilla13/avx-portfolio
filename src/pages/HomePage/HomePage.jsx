8dbcc6703f23885f109fd97ebe1250be43f12bf5
avilla13 <xetox13@gmail.com>
2023-11-30 23:24:16 -0600

    Change HomePage.jsx info

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello! I'm Alejandro Villalobos (Alex for short).</h1>
+      <h1>Hello! I'm Alex Villalobos</h1>
       <h2>Welcome to my portfolio!</h2>
       <main>
         <p>
-          Hello! I'm a passionate software developer specializing in web development.
+          I'm a passionate software developer specializing in web development.
         </p>
       </main>
     </div>
