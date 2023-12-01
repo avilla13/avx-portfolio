3cab10e9c999bb98ae3495c86397f7179f2939e3
avilla13 <xetox13@gmail.com>
2023-11-30 23:04:13 -0600

    Add HomePage.css and basic HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,11 @@ export default function HomePage() {
     <div>
       <h1>Hello! I'm Alejandro Villalobos (Alex for short).</h1>
       <h2>Welcome to my portfolio!</h2>
+      <main>
+        <p>
+          Hello! I'm a passionate software developer specializing in web development.
+        </p>
+      </main>
     </div>
   )
 }
