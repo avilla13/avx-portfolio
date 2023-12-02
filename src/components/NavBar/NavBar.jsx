f014ed36721bef33aecb5d374dcb3bff1e905ad2
avilla13 <xetox13@gmail.com>
2023-12-01 22:43:56 -0600

    Add classes for icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,31 +15,31 @@ export default function Navbar() {
     return (
         <nav>
             <button onClick={toggleSidebar} className='navtoggle'>
-                <FontAwesomeIcon icon={ faBars } size='lg' style={{color: "white",}} />
+                <FontAwesomeIcon icon={ faBars } size='lg' style={{color: "white",}} className="nav-icon" />
             </button>
             <div className={`navitems ${openNav ? 'open' : 'closed'}`} >
                 <ul>
                     <li>
                         <Link to="/" className='nav-home'>
-                            <FontAwesomeIcon icon={faHouse} size="sm" style={{color: "white",}} />
+                            <FontAwesomeIcon icon={faHouse} size="sm" style={{color: "white",}} className="nav-icon" />
                             {openNav && <span> Home</span>}
                         </Link>
                     </li>
                     <li>
                         <Link to="/about" className='nav-about'>
-                            <FontAwesomeIcon icon={faAddressCard} size="sm" style={{color: "white",}} />
+                            <FontAwesomeIcon icon={faAddressCard} size="sm" style={{color: "white",}} className="nav-icon" />
                             {openNav && <span> About</span>}
                         </Link>
                     </li>
                     <li>
                         <Link to="/projects" className='nav-projects'>
-                            <FontAwesomeIcon icon={faBriefcase} size="sm" style={{color: "white",}} />
+                            <FontAwesomeIcon icon={faBriefcase} size="sm" style={{color: "white",}} className="nav-icon" />
                             {openNav && <span> Projects</span>}
                         </Link>
                     </li>
                     <li>
                         <a href="/contact" className='nav-contact'>
-                            <FontAwesomeIcon icon={faUserTie} size='sm' style={{color: "white",}} />
+                            <FontAwesomeIcon icon={faUserTie} size='sm' style={{color: "white",}} className="nav-icon" />
                             {openNav && <span> Contact</span>}
                         </a>
                     </li>
