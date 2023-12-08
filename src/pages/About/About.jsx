adeabd7d18c866c79e6cba4ec2c51658c8f33baf
avilla13 <xetox13@gmail.com>
2023-12-08 00:40:14 -0600

    Update About.jsx hr

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,13 +4,13 @@ import './About.css';
 export default function About() {
   return (
     <div>
-      <br />
+      <hr />
       <h2>About Me: </h2>      
       <main>
         <p>
           I'm a full-stack engineer who is passionate about problem solving. I am transitioning from 7 years of manufacturing engineering where I specialized in custom fabrication. I love knowing how things come together and come apart, whether it's a refrigerated counter or a dynamic user responsive web app. I thrive in a team work-environment because I'm firm believer in communication.
         </p>
-        <br />
+        <hr />
       </main>
     </div>
   )
