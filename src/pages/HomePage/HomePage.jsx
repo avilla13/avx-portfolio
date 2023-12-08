8ea1f605e1937ea6d24b512b5d8f6e0cab91b4c9
avilla13 <xetox13@gmail.com>
2023-12-07 23:43:38 -0600

    Update Homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,12 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Hello! I'm Alex Villalobos</h1>
-      <h2>Welcome to my portfolio!</h2>
-      <main>
-        <p>
-          I'm a passionate software developer specializing in web development.
-        </p>
-      </main>
+      <h2>Welcome to my portfolio!</h2>      
     </div>
   )
 }
