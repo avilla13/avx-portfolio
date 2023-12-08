927b0456be0e07674715c2c1f9e69156a5b75cf7
avilla13 <xetox13@gmail.com>
2023-12-07 23:54:59 -0600

    Update About page info

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      <h1>Hello! I'm Alex Villalobos</h1>
-      <h2>Welcome to my portfolio!</h2>
+      <br />
+      <h2>About Me: </h2>      
       <main>
         <p>
-          I'm a passionate software developer specializing in web development.
+          I'm a full-stack engineer who is passionate about problem solving. I am transitioning from 7 years of manufacturing engineering where I specialized in custom fabrication. I love knowing how things come together and come apart, whether it's a refrigerated counter or a dynamic user responsive web app. I thrive in a team work-environment because I'm firm believer in communication.
         </p>
+        <br />
       </main>
     </div>
   )
